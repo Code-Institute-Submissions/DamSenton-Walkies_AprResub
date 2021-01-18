a67ed2f0646d1c57e035cf47d4d902774ebd719d
Samuel Denton <samdenton1995@hotmail.co.uk>
2021-01-18 15:24:39 +0000

    added requirements.txt, Procfile, and multiple tamplates to my app, in order to work on these at a later date

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import os
-from flask import Flask, render_template
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
@@ -18,6 +25,21 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/sign_in")
+def sign_in_user():
+    return render_template("sign_in.html")
+
+
+@app.route("/register_owner")
+def register_owner():
+    return render_template("register_owner.html")
+
+
+@app.route("/register_walker")
+def register_walker():
+    return render_template("register_walker.html")
 
 
 if __name__ == "__main__":
